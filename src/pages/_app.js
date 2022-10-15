ff3689f12885ef30669c4a63821e8cc9c3c7a35f
Gustavo Oliveira <dogustavo.oliveira@gmail.com>
2022-10-15 10:52:43 -0300

    feat: Adicionado componente padrão de navigation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <Component {...pageProps} />
     </>
   );
